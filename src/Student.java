446159e6e7ac3d8aa731bae6566b4f17ff289f5c
abdihalim-hassen-ahmed <abdihalimhassenahmed@gmail.com>
2026-03-19 21:14:42 +0300

    initial-class-setup
    initial class setup

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String name;
+    private int studentId;
+
+    // Constructor
+    public Student(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    
+}
